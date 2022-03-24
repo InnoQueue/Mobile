efe1935efbff1b081ced34a592cf1d9a3b888c3d
Timur Nugaev <decter49pro@gmail.com>
2022-03-24 21:07:44 +0300

    add notifications back

--- lib/features/notifications/page/notifications_page.dart
+++ lib/features/notifications/page/notifications_page.dart
@@ -1,22 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:inno_queue/const/appres.dart';
 import 'package:inno_queue/core/api/api_notifications.dart';
+import 'package:inno_queue/features/notifications/model/notification_model.dart';
+import 'package:inno_queue/features/notifications/widget/notifications_view.dart';
 
-class NotificationsPage extends StatelessWidget {
+class NotificationsPage extends StatefulWidget {
   const NotificationsPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    ApiNotificationsService.getNotifications();
+  State<NotificationsPage> createState() => _NotificationsPageState();
+}
 
-    return const SafeArea(
-      child: Center(
-          child: Text(
-        AppRes.notifications,
-        style: TextStyle(
-          fontSize: 40,
-        ),
-      )),
+class _NotificationsPageState extends State<NotificationsPage> {
+  late Future<List<List<NotificationModel>>> future;
+
+  @override
+  void initState() {
+    future = ApiNotificationsService.getNotifications();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: FutureBuilder(
+        future: future,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (!snapshot.hasData) return SizedBox.shrink();
+          var notifications = snapshot.data;
+          return Column(
+            children: <Widget>[
+              NotificationsView(notifications[0]),
+              NotificationsView(notifications[1]),
+              const Center(
+                child: Text(
+                  AppRes.notifications,
+                  style: TextStyle(
+                    fontSize: 40,
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
