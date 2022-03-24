66cf06bb513bdfcb7d3e34aa20aa9710e2424628
Timur Nugaev <decter49pro@gmail.com>
2022-03-24 19:37:17 +0300

    api settings for working with settings basic layout of settings page

--- lib/features/settings/page/settings_page.dart
+++ lib/features/settings/page/settings_page.dart
@@ -1,19 +1,42 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:inno_queue/const/appres.dart';
+import 'package:inno_queue/core/api/api_settings.dart';
 
 class SettingsPage extends StatelessWidget {
+  final String defaultName = "Unnamed #41";
+
+  // final Map<String, dynamic> defaultBody = {
+  //
+  // };
+
   const SettingsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Center(
-          child: Text(
-        AppRes.settings,
-        style: TextStyle(
-          fontSize: 40,
+        child: Column(
+          children: [
+            ListView.builder(
+              itemBuilder: (context, index) => CupertinoSwitch(
+                value: false,
+                onChanged: (bool changedValue) => _onChanged("n${index+1}", changedValue),
+              ),
+            ),
+            Text(
+              AppRes.settings,
+              style: TextStyle(
+                fontSize: 40,
+              ),
+            ),
+          ],
         ),
-      )),
+      ),
     );
+  }
+
+  void _onChanged(String index, dynamic changedValue) {
+    // ApiSettingsService.setModel(defaultBody, toSet)
   }
 }
